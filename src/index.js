1a033510c3411a57954c7cacbd6319a0806f5284
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2021-03-05 20:18:27 +0600

    bug fixed

--- src/index.js
+++ src/index.js
@@ -9,13 +9,14 @@ function clock() {
   const element = (
     <main>
       <h1> Digital Clock</h1>
-      <h1 className='datebox'>Date: {new Date().getDate()} - {new Date().getMonth()} - {new Date().getFullYear()}</h1>
+      <h1 className='datebox'>Date: {new Date().getDate()} - {new Date().getMonth() + 1 } - {new Date().getFullYear()}</h1>
       <div className='App'>
         <h1>{new Date().getHours()}</h1>&nbsp;<strong>:</strong>&nbsp;
         <h1>{new Date().getMinutes()}</h1>&nbsp;<strong>:</strong>&nbsp;
         <h1>{new Date().getSeconds()}</h1>
         {/* <h2>It is {new Date().toLocaleTimeString()}.</h2> */}
       </div>
+      <footer>&copy;&nbsp;<a href="https://akhlak-hossain-jim.github.io/" target="_blank" rel="noopener noreferrer">Akhlak Hossain Jim</a>&nbsp;{new Date().getFullYear()}</footer>
     </main>
   );
   ReactDOM.render(element, document.getElementById('root'));
